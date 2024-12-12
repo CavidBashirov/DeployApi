8e683fed649af82c72dc75565165985aa2c99b4b
Cavid Bashirov <bashirovcavid93gmail.com>
2024-12-12 10:23:38 +0400

    added last changes

---
+++ DeployApi/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeployApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<string> students = new()
+            { "Eldar","Fadile","Samir","Nergiz","Elshad","Ferdi","Valeh","Aqil","Terxan","Nihat","Tukezban"};
+
+            return Ok(students);
+        }
+    }
+}
